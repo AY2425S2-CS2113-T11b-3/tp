34c23aa549562fe37fba95a7ffa5f562332c726b
tzqgav10 <e1155947@u.nus.edu>
2025-03-12 21:57:58 +0800

    Add method to print patient information

--- src/main/java/patient/Patient.java
+++ src/main/java/patient/Patient.java
@@ -25,8 +25,20 @@ public class Patient {
         patientsList.remove(index - 1);
     }
 
+    public static void printPatientInformation() {
+        if (patientsList.isEmpty()) {
+            System.out.println("Patient information is empty.");
+            return;
+        }
+        System.out.println("Patient information:");
+        System.out.println("");
+        for (int index = 0; index < patientsList.size(); index++) {
+            System.out.println((index + 1) + ". " + patientsList.get(index).toString());
+        }
+    }
+
     @Override
     public String toString() {
-        return "| " + name + " | " + age + " years old" + " | " + notes + " |";
+        return name + ", " + age + " years old" + ", " + notes + ".";
     }
 }
