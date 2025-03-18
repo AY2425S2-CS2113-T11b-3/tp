8f0066116ea5b9a0d7fa9f2406d7e3f432f78e72
Isaaclks7 <e1122338@u.nus.edu>
2025-03-18 14:01:49 +0800

    Remove unused imports from NurseSched class

--- src/main/java/NurseSched.java
+++ src/main/java/NurseSched.java
@@ -2,14 +2,6 @@ import java.util.Scanner;
 
 import command.Command;
 import ui.Ui;
-import appointment.Appointment;
-import exception.NurseSchedException;
-import parser.ShiftParser;
-import parser.ApptParser;
-import parser.Parser;
-import parser.PatientParser;
-import patient.Patient;
-import shift.Shift;
 
 public class NurseSched {
     private final Ui ui;
